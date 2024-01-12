d7e729cbc3a94df709664028ca4fb903d9bf551f
Raju <rajneduri123@gmail.com>
2024-01-12 22:29:41 +0530

    array program

--- src/class_11_01_Array/Array1.java
+++ src/class_11_01_Array/Array1.java
@@ -4,7 +4,14 @@ public class Array1 {
 
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4, 5, 6, 78, 99 };
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
 		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println(arr[5]);
+		System.out.println(arr[6]);
+		System.out.println(arr[7]);
 	}
 
 }
