ea5c6f54546dd39865d4e883cddbe389c5c740ed
Raju <rajneduri123@gmail.com>
2024-01-12 22:25:35 +0530

    pass

---
+++ src/class_11_01_Array/Array1.java
@@ -0,0 +1,10 @@
+package class_11_01_Array;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 6, 78, 99 };
+		System.out.println(arr[2]);
+	}
+
+}
