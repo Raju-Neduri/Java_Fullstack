7a2ba84cf0f0054818e1ba94977a17555292be25
Raju <rajneduri123@gmail.com>
2024-01-13 13:22:01 +0530

    Array min and max commit

---
+++ src/class_11_01_Array/Min_And_Max_Array.java
@@ -0,0 +1,34 @@
+//write a program to take input of the array elements and check the minimum and maximum
+package class_11_01_Array;
+
+import java.util.Scanner;
+
+public class Min_And_Max_Array {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int n = sc.nextInt();
+
+		int num[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter element at index " + i + ":");
+			num[i] = sc.nextInt();
+
+		}
+		int min = num[0];
+		int max = num[0];
+		for (int i = 1; i <= num.length; i++) {
+			if (num.length < min) {
+				min = num[i];
+			} else if (num.length > max) {
+				max = num[i];
+			}
+		}
+		System.out.println();
+		System.out.println("Minimum: " + min);
+		System.out.println("Maximum: " + max);
+	}
+
+}
