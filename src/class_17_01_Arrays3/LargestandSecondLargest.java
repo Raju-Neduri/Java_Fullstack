fc9b882056e8d352590c9d30041981949c7303fb
Raju <rajneduri123@gmail.com>
2024-01-17 19:27:28 +0530

    min and max

--- src/class_17_01_Arrays3/LargestandSecondLargest.java
+++ src/class_17_01_Arrays3/LargestandSecondLargest.java
@@ -2,7 +2,7 @@ package class_17_01_Arrays3;
 
 import java.util.Scanner;
 
-public class MinAndMaxArrayEx {
+public class LargestandSecondLargest {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,20 +16,24 @@ public class MinAndMaxArrayEx {
 			num[i] = sc.nextInt();
 
 		}
-		int min = num[0];
-		int max = num[0];
+		int largest = num[0];
+		int secondLargest = num[0];
 		for (int i = 1; i < n; i++) {
-			if (num[i] > max) {
-				max = num[i];
-			} else if (num[i] < min) {
-				min = num[i];
-
+			if (num[i] > largest) {
+				largest = num[i];
 			}
+		}
 
+		for (int j = 1; j < n; j++) {
+			if (num[j] > secondLargest) {
+				if (secondLargest != largest) {
+					secondLargest = num[j];
+				}
+			}
 		}
 		System.out.println();
-		System.out.println("Minimum: " + min);
-		System.out.println("Maximum: " + max);
+		System.out.println("Largest: " + largest);
+		System.out.println("Second largest: " + secondLargest);
 	}
 
 }
