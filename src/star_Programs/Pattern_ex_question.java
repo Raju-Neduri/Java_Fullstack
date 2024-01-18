1b40c871a60c4d093aa03594476547f662f8b61a
Raju <rajneduri123@gmail.com>
2024-01-18 23:08:16 +0530

    fail

--- src/star_Programs/Pattern_ex_question.java
+++ src/star_Programs/Pattern_ex_question.java
@@ -13,19 +13,17 @@ package star_Programs;
 public class Pattern_ex_question {
 
 	public static void main(String[] args) {
-		int n = 5;
+		int n = 3;
+
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
-				if (j % 2 != 0) {
-					System.out.print("*");
+				if (j % 2 == 0) {
+					System.out.print("* ");
 				} else {
-					System.out.print(" ");
+					System.out.print("  ");
 				}
 			}
-
 			System.out.println();
 		}
-
 	}
-
 }
