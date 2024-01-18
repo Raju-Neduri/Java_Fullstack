1a77e9b8d26d3dc000a05d55f9442d7907054b3e
Raju <rajneduri123@gmail.com>
2024-01-18 23:08:52 +0530

    pattern

--- src/star_Programs/Pattern_ex_question.java
+++ src/star_Programs/Pattern_ex_question.java
@@ -17,7 +17,7 @@ public class Pattern_ex_question {
 
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
-				if (j % 2 == 0) {
+				if (j % 2 == 1) {
 					System.out.print("* ");
 				} else {
 					System.out.print("  ");
