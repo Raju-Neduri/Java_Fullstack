5fd82f712089dfd5ac7b27c5256bdfad641aee82
Raju <rajneduri123@gmail.com>
2024-01-18 23:17:10 +0530

    failed

---
+++ src/class_18_01_jaggedArrays/JaggedArrayEX2.java
@@ -0,0 +1,18 @@
+package class_18_01_jaggedArrays;
+
+import java.util.Scanner;
+
+public class JaggedArrayEX2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the no.of Rows");
+		int rows = sc.nextInt();
+
+		for (int i = 0; i < rows; i++) {
+		}
+
+	}
+
+}
