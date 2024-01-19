52e64d0e6232df9b78108490ee7121f2601d4828
Raju <rajneduri123@gmail.com>
2024-01-19 17:33:28 +0530

    pattern sloved

--- src/star_Programs/Pattern_ex_question.java
+++ src/star_Programs/Pattern_ex_question.java
@@ -15,15 +15,27 @@ public class Pattern_ex_question {
 	public static void main(String[] args) {
 		int n = 3;
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i < n; i++) {
 			for (int j = 1; j <= i; j++) {
-				if (j % 2 == 1) {
-					System.out.print("* ");
+				if ((i + j) % 2 == 0) {
+					System.out.print("*");
 				} else {
-					System.out.print("  ");
+					System.out.print(" ");
 				}
 			}
 			System.out.println();
 		}
+
+		for (int i = n; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				if ((i + j) % 2 == 0) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
 	}
 }
