24099487a885348beba5b5236e245784886a07f0
Raju <rajneduri123@gmail.com>
2024-01-19 19:51:14 +0530

    String introduction

---
+++ src/class_19_01_string/String_Ex1.java
@@ -0,0 +1,17 @@
+package class_19_01_string;
+
+public class String_Ex1 {
+
+	public static void main(String args[]) {
+		String s1 = "Raju";
+
+		char ch[] = { 'N', 'E', 'D', 'U', 'R', 'I' };
+		String s2 = new String(ch);
+
+		String s3 = new String("Ashok IT");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+	}
+}
