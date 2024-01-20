f60a4f8f6f014564a13f08a3a4ff0daf5c537fdb
Raju <rajneduri123@gmail.com>
2024-01-20 21:50:34 +0530

    String 4

---
+++ src/class_19_01_string/String_ex4.java
@@ -0,0 +1,12 @@
+package class_19_01_string;
+
+public class String_ex4 {
+	public static void main(String args[]) {
+		String s = new String("Rajuking");
+		char c = s.charAt(0);
+		System.out.println(c);
+		int length = s.length();
+
+	}
+
+}
