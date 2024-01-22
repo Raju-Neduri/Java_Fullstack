622b2bda3407446c9a7a5aa6cfd04f777a6e22d1
Raju <rajneduri123@gmail.com>
2024-01-22 21:49:40 +0530

    String index

---
+++ src/class_22_01_String2/String_indexOf2.java
@@ -0,0 +1,13 @@
+package class_22_01_String2;
+
+public class String_indexOf2 {
+
+	public static void main(String[] args) {
+
+		String s1 = "This is indexOf method";
+		// Passing Substring
+		int index = s1.indexOf("method"); // Returns the index of this substring
+		System.out.println("index of substring " + index);
+	}
+
+}
