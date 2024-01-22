a9afbfea31249f521ec9860d97aeb196d2c0bb0a
Raju <rajneduri123@gmail.com>
2024-01-22 21:03:06 +0530

    string replace

---
+++ src/class_22_01_String2/StringExamples.java
@@ -0,0 +1,15 @@
+//using replace method
+
+package class_22_01_String2;
+
+public class StringExamples {
+
+	public static void main(String[] args) {
+		String s = "welcome to AshokIT";
+		String replace = s.replace("AshokIT", "Hyd");
+		System.out.println(s);
+		System.out.println(replace);
+
+	}
+
+}
