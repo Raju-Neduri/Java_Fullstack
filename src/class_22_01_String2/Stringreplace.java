227fb4855ec0b7d2e3ab2f96b16aa7faf93af16f
Raju <rajneduri123@gmail.com>
2024-01-22 09:14:20 +0530

    string class 2 example 1

---
+++ src/class_22_01_String2/Stringreplace.java
@@ -0,0 +1,19 @@
+package class_22_01_String2;
+
+public class Stringreplace {
+	public static void main(String args[]) {
+		String s = "AshokIT";
+		String s1 = "AshokIT";
+		String s2 = new String("AshokIT");
+
+		System.out.println("Equals method");
+		System.out.println(s1.equals(2));
+		System.out.println(s.equals(s1));
+
+		System.out.println("=============================");
+		System.out.println(s1 == s2);
+		System.out.println(s == s1);
+
+	}
+
+}
