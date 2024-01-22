2d9d2663d08487c3700f6203cd3e5c88fc2451a9
Raju <rajneduri123@gmail.com>
2024-01-22 21:12:12 +0530

    string contains

---
+++ src/class_22_01_String2/String_CharAt.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * The index number starts from 0 and goes to n-1, where n is the length of the string.
+
+
+
+*/
+
+package class_22_01_String2;
+
+public class String_CharAt {
+
+	public static void main(String[] args) {
+		String s = "Pain never ends";
+		char ch = s.charAt(3);//
+
+		System.out.println(ch);
+	}
+
+}
---
+++ src/class_22_01_String2/String_contains.java
@@ -0,0 +1,13 @@
+//String contains examples
+package class_22_01_String2;
+
+public class String_contains {
+
+	public static void main(String args[]) {
+		String name = "What you know about me";
+		System.out.println(name.contains("you"));
+		System.out.println(name.contains("me"));
+		System.out.println(name.contains("Raju"));
+
+	}
+}
