f17360f5e55855ffbba47792e39699015f2130ed
Raju <rajneduri123@gmail.com>
2024-01-22 20:55:15 +0530

    string

---
+++ src/class_22_01_String2/StringIndexOf.java
@@ -0,0 +1,11 @@
+package class_22_01_String2;
+
+public class StringIndexOf {
+
+	public static void main(String[] args) {
+
+		String s1 = "Raju";
+		String s2 = "Neduri";
+	}
+
+}
---
+++ src/class_22_01_String2/Sub_string.java
@@ -0,0 +1,13 @@
+package class_22_01_String2;
+
+public class Sub_string {
+
+	public static void main(String[] args) {
+
+		String s = "welcome to AshokIT";
+		System.out.println(s.substring(3));
+		System.out.println(s.substring(3, 7));
+
+	}
+
+}
