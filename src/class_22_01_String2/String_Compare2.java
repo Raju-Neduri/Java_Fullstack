56365b397e4575f738ecb0dce9f2d06c2dd434b4
Raju <rajneduri123@gmail.com>
2024-01-23 10:45:15 +0530

    string compare 2

---
+++ src/class_22_01_String2/String_Compare2.java
@@ -0,0 +1,20 @@
+package class_22_01_String2;
+
+public class String_Compare2 {
+
+	public static void main(String[] args) {
+		String s1 = "Raju";
+		String s2 = "Raju";
+		String s3 = "Rama";
+
+		String s4 = "apple";
+		String s5 = "bannana";
+		String s6 = "Grapes";
+		String s7 = "Apple";
+		System.out.println(s5.compareTo(s4));
+		System.out.println(s6.compareTo(s5));
+		System.out.println(s4.compareTo(s7));
+
+	}
+
+}
