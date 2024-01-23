b23eff3e92b27024489c44cfecfa177e60b34855
Raju <rajneduri123@gmail.com>
2024-01-23 17:22:00 +0530

    problem 3

--- src/class_22_01_String2/P3_CompareToIgnore.java
+++ src/class_22_01_String2/P3_CompareToIgnore.java
@@ -1,9 +1,19 @@
+//3. write a java program to compare two strings lexicographically, ignore case difference
 package class_22_01_String2;
+
+import java.util.Scanner;
 
 public class P3_CompareToIgnore {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String s1 = sc.next();
+
+		System.out.println("Enter the second string");
+		String s2 = sc.next();
+
+		System.out.println(s1.compareToIgnoreCase(s2));
 
 	}
 
