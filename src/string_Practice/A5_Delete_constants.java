82be3080f45ce7c65158018558460fa7b279769d
Raju <rajneduri123@gmail.com>
2024-01-24 20:57:11 +0530

    Delete constants in string

--- src/string_Practice/A5_Delete_constants.java
+++ src/string_Practice/A5_Delete_constants.java
@@ -1,9 +1,12 @@
-//Assignment 5
+//Assignment 5 - Write a java program to delete all constants from  the string "Hello, have a good day".
 package string_Practice;
 
 public class A5_Delete_constants {
 
 	public static void main(String[] args) {
+		String s = "Hello, have a good day";
+		String s1 = s.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]", "");
+		System.out.println(s1);
 
 	}
 
