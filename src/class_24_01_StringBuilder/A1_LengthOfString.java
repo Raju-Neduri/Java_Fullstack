eb7ef8ae2dbabb674351ac65f70f1a54b6f4c09d
Raju <rajneduri123@gmail.com>
2024-01-24 13:08:25 +0530

    length of the string

---
+++ src/class_24_01_StringBuilder/A1_LengthOfString.java
@@ -0,0 +1,12 @@
+// Assignment 1. Wap to find the length of the string Refrigerator
+
+package class_24_01_StringBuilder;
+
+public class A1_LengthOfString {
+
+	public static void main(String[] args) {
+		String s = "refrigerator";
+		System.out.println(s.length());
+	}
+
+}
