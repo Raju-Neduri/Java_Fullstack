18dc62a785497ca0f111df22f11919152d85864a
Raju <rajneduri123@gmail.com>
2024-01-24 19:51:19 +0530

    Assignment 2 and 3

---
+++ src/string_Practice/A2_Letter_E.java
@@ -0,0 +1,12 @@
+// Assignment 2-Wap to check if the letter 'e' is present in the string word "Umbrella" ? 
+
+package string_Practice;
+
+public class A2_Letter_E {
+
+	public static void main(String[] args) {
+		String s = "Umbrella";
+		System.out.println(s.contains("e"));
+	}
+
+}
---
+++ src/string_Practice/A3_Word_Orange_Present.java
@@ -0,0 +1,11 @@
+//Assignement 3- Wap to check if the word "orange" is present in the "this is orange juice".
+package string_Practice;
+
+public class A3_Word_Orange_Present {
+
+	public static void main(String[] args) {
+		String s = "This is orange juice";
+		System.out.println(s.contains("orange"));//
+	}
+
+}
