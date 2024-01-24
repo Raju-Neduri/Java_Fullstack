9c2c95925d75698c618d95dd140f1524fcea9adb
Raju <rajneduri123@gmail.com>
2024-01-24 07:17:49 +0530

    Reverse String

---
+++ src/class_22_01_String2/Reverse_String_23_01.java
@@ -0,0 +1,18 @@
+package class_22_01_String2;
+
+public class Reverse_String_23_01 {
+
+	public static void main(String[] args) {
+		String str = "Raju Neduri";
+		String s = "";
+		int i, len;
+		len = str.length();
+
+		for (i = len - 1; i >= 0; i--) {
+			s = s + str.charAt(i);
+
+		}
+		System.out.println(s);
+	}
+
+}
