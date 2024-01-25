fb094f925a0a12cf8505855068262f950d975e9d
Raju <rajneduri123@gmail.com>
2024-01-25 08:50:01 +0530

    Palindrome

---
+++ src/string_Practice/A12_StringPalindrome.java
@@ -0,0 +1,12 @@
+// 12.Wap to check whether the given string is palindrome or not ?
+//(Hint : original string and reverse of a string are both are same Ex:Madam-- madam
+package string_Practice;
+
+public class A12_StringPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
