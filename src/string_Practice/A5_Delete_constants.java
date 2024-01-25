ad28bc8cf33ec52e233c325f71b1b1172c4f2ce5
Raju <rajneduri123@gmail.com>
2024-01-25 06:04:34 +0530

    delete constants

--- src/string_Practice/A5_Delete_constants.java
+++ src/string_Practice/A5_Delete_constants.java
@@ -1,11 +1,11 @@
-//Assignment 5 - Write a java program to delete all constants from  the string "Hello, have a good day".
+//Assignment 5 - Write a java program to delete all consonants from  the string "Hello, have a good day".
 package string_Practice;
 
 public class A5_Delete_constants {
 
 	public static void main(String[] args) {
 		String s = "Hello, have a good day";
-		String s1 = s.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]", "");
+		String s1 = s.replaceAll("[^aeiouAEIOU]", "");// this expression is used to del all other consonants
 		System.out.println(s1);
 
 	}
