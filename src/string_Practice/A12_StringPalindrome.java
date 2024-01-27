9241d96f3fca90dad8fd58aab0113b75513665c2
Raju <rajneduri123@gmail.com>
2024-01-27 09:14:16 +0530

    String palindrome

--- src/string_Practice/A12_StringPalindrome.java
+++ src/string_Practice/A12_StringPalindrome.java
@@ -1,11 +1,26 @@
 // 12.Wap to check whether the given string is palindrome or not ?
+
 //(Hint : original string and reverse of a string are both are same Ex:Madam-- madam
 package string_Practice;
+
+import java.util.Scanner;
 
 public class A12_StringPalindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
+
+		String reversed = new StringBuilder(str).reverse().toString();
+		boolean result = str.equals(reversed);
+		System.out.println(result);
+
+		if (result) {
+			System.out.println("The string is a palindrome.");
+		} else {
+			System.out.println("The string is not a palindrome.");
+		}
 
 	}
 
