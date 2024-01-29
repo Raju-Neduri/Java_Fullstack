ad3e4743d3f0af6dd5c5ac54188d20d415858b4c
Raju <rajneduri123@gmail.com>
2024-01-29 09:21:50 +0530

    Static method

---
+++ src/class19_29_01_Objec/Static_Method.java
@@ -0,0 +1,37 @@
+package class19_29_01_Objec;
+
+public class Static_Method {
+
+	int id;
+	String name;
+
+	static String comp_name = "TCS";
+
+	public void accept(int id, String name, String comp_name) {
+		this.id = id;
+		this.name = name;
+
+	}
+
+	public void display() {
+		System.out.println("id=" + id + ", name=" + name + ", Company name=" + comp_name);
+	}
+
+	public void show() {
+		System.out.println("\nShowing use of Static keyword\n");
+	}
+
+	public static void main(String[] args) {
+		Static_Method m = new Static_Method();
+		Static_Method m1 = new Static_Method();
+
+		m.accept(1, "Shital", " ");
+		m.display();
+		m.show();
+		m.accept(2, "Raju", " ");
+		m.display();
+		m.show();
+
+	}
+
+}
