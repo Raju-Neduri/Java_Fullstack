d18206af468c69502658dd3d9d9607a64c9ee11f
Raju <rajneduri123@gmail.com>
2024-02-01 09:20:20 +0530

    variable types

--- src/class20_01_Feb_Static/Variable1.java
+++ src/class20_01_Feb_Static/Variable1.java
@@ -2,11 +2,21 @@ package class20_01_Feb_Static;
 
 public class Variable1 {
 	int id;
-	String name;
+	String name;// instance variable
 	String clg;
+	static int position = 100;// static variable
+
+	public void accept(int i, String s, String c) {
+		id = i;// local variable
+
+	}
+
+	public void display() {
+		id = 100;
+
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
