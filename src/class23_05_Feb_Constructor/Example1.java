62247e01681e17b569d3b9cdd8513e790db4bdf6
Raju <rajneduri123@gmail.com>
2024-02-05 09:08:19 +0530

    Constructor

---
+++ src/class23_05_Feb_Constructor/Example1.java
@@ -0,0 +1,19 @@
+package class23_05_Feb_Constructor;
+
+public class Example1 {
+
+	int id;
+	String name;
+
+	public Example1() {
+		System.out.println("Default contructor");
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Inside the main method");
+		Example1 e = new Example1();
+
+	}
+
+}
