a5fe734f462ab92e9a8078c99a3097470900a5a3
Raju <rajneduri123@gmail.com>
2024-02-05 09:59:28 +0530

    vechile

---
+++ src/class23_05_Feb_Constructor/Vechile.java
@@ -0,0 +1,13 @@
+package class23_05_Feb_Constructor;
+
+public class Vechile {
+	int modelno;
+	String name;
+	int price;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
