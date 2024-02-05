c5722bb5d35941682a81dda455af87873d622587
Raju <rajneduri123@gmail.com>
2024-02-05 19:43:53 +0530

    questions on class

--- src/classOjectsMethods/Question2.java
+++ src/classOjectsMethods/Question2.java
@@ -2,14 +2,27 @@
 //
 //Instantiate an object of the Person class and set the name to "Alice" and age to 25.
 //Use the introduce method to print the person's introduction.
-
 package classOjectsMethods;
 
-public class Question2 {
+class Person1 {
+	String name;
+	int age;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	void introduce() {
+		System.out.println("Hello " + name + ", welcome! Your age is: " + age);
 	}
+}
 
+public class Question2 {
+	public static void main(String[] args) {
+		// Instantiate an object of the Person class
+		Person1 person = new Person1();
+
+		// Set the name and age
+		person.name = "Alice";
+		person.age = 25;
+
+		// Use the introduce method to print the person's introduction
+		person.introduce();
+	}
 }
---
+++ src/classOjectsMethods/Question3.java
@@ -0,0 +1,19 @@
+/*
+Question 3: Adding a Method
+
+Add a method have_birthday to the Person class that increments the age by 1. 
+Instantiate a person object, call the have_birthday method, and 
+then use the introduce method to print the updated information.
+
+*/
+
+package classOjectsMethods;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
