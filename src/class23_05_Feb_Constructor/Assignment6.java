cf060779513d331430b1445078a97e7086744677
Raju <rajneduri123@gmail.com>
2024-02-06 12:38:07 +0530

    assignment 6

--- src/class23_05_Feb_Constructor/Assignment6.java
+++ src/class23_05_Feb_Constructor/Assignment6.java
@@ -1,11 +1,27 @@
+/*
+ * write a java program to print the names of students by creating  a student  class
+ *  if no name is passed  while creating an object of a student class then the name should be
+ *  "unknown" . otherwise  the name should be equal  to the string  value passed while creating
+ *  object of student class
+ */
+
 package class23_05_Feb_Constructor;
 
-class student {
+class Student {
 	String name;
-	int age;
 
-	student(String name, int age) {
+	// Constructor with default value "unknown" if no name is passed
+	public Student() {
+		this.name = "unknown";
+	}
 
+	// Constructor with parameter to set the name if provided
+	public Student(String name) {
+		this.name = name;
+	}
+
+	public void displayName() {
+		System.out.println("Student Name: " + name);
 	}
 
 }
@@ -13,6 +29,11 @@ class student {
 public class Assignment6 {
 
 	public static void main(String[] args) {
+
+		Student student1 = new Student();
+		Student student2 = new Student("Vamshi");
+		student1.displayName();
+		student2.displayName();
 
 	}
 
