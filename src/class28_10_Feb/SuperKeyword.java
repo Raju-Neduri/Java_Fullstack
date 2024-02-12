5631d61bdcb0e3073a65235d25b3494eaf297f9f
Raju <rajneduri123@gmail.com>
2024-02-12 22:33:07 +0530

    changes

--- src/class28_10_Feb/SuperKeyword.java
+++ src/class28_10_Feb/SuperKeyword.java
@@ -1,7 +1,16 @@
+/*
+ * The super keyword refers to superclass (parent) objects.
+
+It is used to call superclass methods, and to access the superclass constructor.
+
+The most common use of the super keyword is to eliminate the confusion between 
+super classes and subclasses that have methods with the same name.
+ */
 package class28_10_Feb;
 
 class A {
 	int id = 100;
+	String n = "Raj";
 
 	public void show() {
 		System.out.println("Inside the parent class");
@@ -10,6 +19,7 @@ class A {
 
 class B extends A {
 	int id = 200;
+	String n = "Ram";
 
 	public void show() {
 		super.show();
@@ -19,6 +29,7 @@ class B extends A {
 
 	public void display() {
 		System.out.println("child ID:" + id + ", Parent class ID:" + super.id);
+		System.out.println("child String:" + n + " , Parent class name:" + super.n);
 
 	}
 }
