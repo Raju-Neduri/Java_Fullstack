189f2c3549246b8872ee8911faebf4bfa161f182
Raju <rajneduri123@gmail.com>
2024-02-12 22:25:01 +0530

    java

--- src/class28_10_Feb/SuperKeyword.java
+++ src/class28_10_Feb/SuperKeyword.java
@@ -1,15 +1,24 @@
 package class28_10_Feb;
 
 class A {
+	int id = 100;
+
 	public void show() {
 		System.out.println("Inside the parent class");
 	}
 }
 
 class B extends A {
+	int id = 200;
+
 	public void show() {
 		super.show();
 		System.out.println("inside the child class");
+
+	}
+
+	public void display() {
+		System.out.println("child ID:" + id + ", Parent class ID:" + super.id);
 
 	}
 }
@@ -19,6 +28,7 @@ public class SuperKeyword {
 	public static void main(String[] args) {
 		B b = new B();
 		b.show();
+		b.display();
 
 	}
 
