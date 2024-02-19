5af566e74aa6cc41827a84a9a4a380c73f87b960
Raju <rajneduri123@gmail.com>
2024-02-19 09:11:40 +0530

    java

--- src/class32_15_Feb_Inerface/InterfacePlayerEX.java
+++ src/class32_15_Feb_Inerface/InterfacePlayerEX.java
@@ -3,7 +3,6 @@ package class32_15_Feb_Inerface;
 public class InterfacePlayerEX {
 
 	public static void main(String[] args) {
-;aal
 	}
 
 }
--- src/class32_15_Feb_Inerface/VehicleExample.java
+++ src/class32_15_Feb_Inerface/VehicleExample.java
@@ -1,6 +1,6 @@
 package class32_15_Feb_Inerface;
 
-class vehicle {
+interface Vehicle {
 	void changeGear(int x);
 
 	void speedUp(int x);
@@ -8,33 +8,65 @@ class vehicle {
 	void applyBrakes(int a);
 }
 
-class Bicycle implements vehicle {
+class Bicycle implements Vehicle {
 	int speed;
 	int gear;
 
 	public Bicycle() {
 		System.out.println("Bicycle Constructor");
-
 	}
 
-	public void ChangeGear(int cg) {
-
+	public void changeGear(int cg) {
 		gear = cg;
-		System.out.println("Gear changed to: " + Gear);
+		System.out.println("Gear changed to: " + gear);
 	}
 
 	public void speedUp(int incr) {
-
 		speed = speed + incr;
 		System.out.println("Speed up: " + speed);
 	}
 
-	public class VehicleExample {
+	public void applyBrakes(int decr) {
+		speed = speed - decr;
+		System.out.println("After applying brakes speed:" + speed);
+	}
+}
 
-		public static void main(String[] args) {
-			
-			Bicycle b=new Bicycle();
-			b.ChangeGear(0);
-		}
+class Bike implements Vehicle {
+	int speed;
+	int gear;
 
+	public Bike() {
+		System.out.println("Bike Constructor");
+	}
+
+	public void changeGear(int cg) {
+		gear = cg;
+		System.out.println("Gear changed to: " + gear);
+	}
+
+	public void speedUp(int incr) {
+		speed = speed + incr;
+		System.out.println("Speed up: " + speed);
+	}
+
+	public void applyBrakes(int decr) {
+		speed = speed - decr;
+		System.out.println("After applying brakes speed:" + speed);
+	}
+}
+
+public class VehicleExample {
+	public static void main(String[] args) {
+		Bicycle b = new Bicycle();
+		b.changeGear(1);
+		b.speedUp(10);
+		b.applyBrakes(8);
+		System.out.println(" ");
+
+		Bike b1 = new Bike();
+		b1.changeGear(10);
+		b1.speedUp(100);
+		b1.applyBrakes(18);
+	}
 }
