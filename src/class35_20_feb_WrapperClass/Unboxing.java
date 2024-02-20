74bf560bf91470e12f95c3fa1b6673617ba8e758
Raju <rajneduri123@gmail.com>
2024-02-20 08:58:59 +0530

    Wrapper class

---
+++ src/class35_20_feb_WrapperClass/Unboxing.java
@@ -0,0 +1,13 @@
+package class35_20_feb_WrapperClass;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		Integer i = new Integer(46);
+		int x = i;
+
+		System.out.println("object:" + i);
+		System.out.println("Primitive data type:" + x);
+	}
+
+}
---
+++ src/class35_20_feb_WrapperClass/WrapperClassEX.java
@@ -0,0 +1,11 @@
+package class35_20_feb_WrapperClass;
+
+public class WrapperClassEX {
+	public static void main(String[] args) {
+		int a = 20;
+		Integer obj = new Integer(a);
+		Integer obj1 = 900;
+
+	}
+
+}
