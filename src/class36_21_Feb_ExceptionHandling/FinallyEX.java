2c5121dcebab1444ab52b25bfc5d64a47ccf687f
Raju <rajneduri123@gmail.com>
2024-02-21 09:29:56 +0530

    Finally

--- src/class36_21_Feb_ExceptionHandling/FinallyEX.java
+++ src/class36_21_Feb_ExceptionHandling/FinallyEX.java
@@ -6,14 +6,23 @@ public class FinallyEX {
 		String s = "Raju";
 		try {
 			int x = Integer.parseInt(s);
-		} catch (Exception e) {
-			System.out.println(e);
-
+		} catch (NumberFormatException e) {
+			System.out.println("NumberFormatException: " + e);
 		} finally {
-
-			System.out.println("This block always get Executed");
+			System.out.println("This block always gets Executed");
 		}
 		System.out.println("Rest of the Code");
-	}
+		System.out.println(" ");
 
+		// Second block
+
+		int a = 20; // assuming 'a' is supposed to be used here
+		try {
+			int res = a / 10;
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticException: " + e);
+		} finally {
+			System.out.println("This block always gets Executed");
+		}
+	}
 }
