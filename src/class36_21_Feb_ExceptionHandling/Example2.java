59e94debcf195471db6f11cf8042180f36656403
Raju <rajneduri123@gmail.com>
2024-02-21 08:59:20 +0530

    Null pointer exception

---
+++ src/class36_21_Feb_ExceptionHandling/Example2.java
@@ -0,0 +1,24 @@
+package class36_21_Feb_ExceptionHandling;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+
+		int x = 100, res;
+		String s = null;
+		try {
+			res = x / 0;
+			System.out.println(s.length());
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		try {
+			System.out.println(s.length());
+
+		} catch (Exception e) {
+			System.out.println("Null pointer Exception Occured");
+		}
+	}
+
+}
