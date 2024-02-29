0da357999d1ddacacf945895151a767f0a9b3c1f
Raju <rajneduri123@gmail.com>
2024-02-29 09:19:33 +0530

    Linked list

---
+++ src/class43_29_feb_CollectionFrame/Lilnkedlist3.java
@@ -0,0 +1,22 @@
+package class43_29_feb_CollectionFrame;
+
+import java.util.LinkedList;
+
+public class Lilnkedlist3 {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> l = new LinkedList<Integer>();
+		l.add(80);
+		l.add(30);
+		l.add(null);
+		l.add(220);
+
+		LinkedList<Integer> l1 = new LinkedList<Integer>();
+		l1.add(80);
+		l1.add(30);
+		l1.add(null);
+		l1.add(220);
+
+	}
+
+}
