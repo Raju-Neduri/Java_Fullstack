5286dd3d473383c74f3b02c8b99eadf5c1090dbe
Raju <rajneduri123@gmail.com>
2024-03-01 09:11:50 +0530

    Stack

---
+++ src/class44_01_March_Stack/StackEX2.java
@@ -0,0 +1,24 @@
+package class44_01_March_Stack;
+
+import java.util.Stack;
+
+public class StackEX2 {
+
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<Integer>();
+
+		System.out.println(s.empty());
+		s.push(10);
+		s.push(34);
+		s.push(45);
+		s.push(100);
+		System.out.println(s);
+		System.out.println(s.empty());
+
+		System.out.println(s.pop()); // removing
+		System.out.println(s.peek()); // show top element but doesn't removing.
+		System.out.println(s);
+
+	}
+
+}
